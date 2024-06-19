114274523b088e23096aa089b67784df3e0e6e0b
akashkhot12 <akashkhot03@gmail.com>
2024-06-19 22:19:44 +0530

    adding navbar

--- Presentation-layer/src/Componets/Header/Navbar.jsx
+++ Presentation-layer/src/Componets/Header/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 export default function Navbar() {
+    const divStyle = {
+        backgroundColor: 'orange',
+      };
   return (
-    <>
-    
-
-
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 Navbar">
+    <div className="navbar" style={divStyle} >
+            <nav className="bg-dark border-gray-900 dark:bg-gray-900 Navbar">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
@@ -23,7 +23,7 @@ export default function Navbar() {
         <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-md">
                 <li>
-                    <a href="#" className="text-gray-900 dark:text-dark hover:underline" aria-current="page">Home</a>
+                    <a href="#" className="text-gray-900 dark:text-dark " aria-current="page">Home</a>
                 </li>
                 <li>
                     <a href="#" className="text-gray-900 dark:text-dark  ">Company</a>
@@ -38,8 +38,6 @@ export default function Navbar() {
         </div>
     </div>
 </nav>
-
-
-    </>
+    </div>
   )
 }
